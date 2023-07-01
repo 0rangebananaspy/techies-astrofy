2f4b7dd73647298e4116db39ba8cd390f4563b3a
Techies <94751583+0rangebananaspy@users.noreply.github.com>
2023-07-01 22:53:28 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Agung Hendro | Personal Portfolio';
+export const SITE_DESCRIPTION = 'Agung Hendro ialah salah staff UPT Teknologi Informasi dan Komunikasi Universitas Jember. Saya berada di divisi Jaringan dan Perangkat Keras. Lebih spesifik, saya di bagian Data Center dengan fokus Pengembangan Aplikasi dan Website.';
