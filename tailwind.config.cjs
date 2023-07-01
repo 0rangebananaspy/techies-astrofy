761c606350dfba88edd40b267a83c3f29cf0018b
Techies <94751583+0rangebananaspy@users.noreply.github.com>
2023-07-01 22:45:05 +0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
 	daisyui: {
 		themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "dark", // name of one of the included themes for dark mode
+		styled: true, // include daisyUI colors and design decisions for all components
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
 }
